9b5d30f0c7743f61c506db1cd249f3af492ffbb3
Jona505 <jonathanarcosreyes@gmail.com>
2025-02-27 09:02:44 -0600

    Ahora los inventarios aparecen en la vista principal

--- StackQR/www/js/GET.js
+++ StackQR/www/js/GET.js
@@ -3,21 +3,21 @@ const itemsCache = {}; // Almacenará el HTML de cada inventario por ID
 
 //Este script utiliza una API diferente para utilizarla como pruebas.
 //Muy probablemente lo convierte en async/await.
-fetch('https://jsonplaceholder.typicode.com/users') //Promeso que obtine los usuarios de la api
+fetch('https://stackqr.bsite.net/api/inventories') //Promeso que obtine los usuarios de la api
 .then(response => { //Agarra la promeso o respuesta
     return response.json() //Transforma en js. Esto tambien es una promesa
 }) 
 .then(data =>{ //Esto agarra la promesa anterior
-    data.forEach(user => { //Loop que pasa por cada usuario y lo almacena en user
+    data.forEach(inventory => { //Loop que pasa por cada usuario y lo almacena en user
         const name = //Con esta variable se guarda la estrutura html que se incrusta en el div con id inventoryList.
         `<div class = "flex-column mb-3">
             <div class = "d-flex flex-row gap-3">
                 <div>
-                <img src="img/icons/carpeta.svg" style="width: 80px; height: 80px;" alt="" data-id="${user.id}"> 
+                <img src="img/icons/carpeta.svg" style="width: 80px; height: 80px;" alt="" data-id="${inventory.id_inventory}"> 
                 </div>
                 
                 <div class= "bg-secondary flex-grow-1">
-                <h3>${user.email}</h3>
+                <h3>${inventory.name}</h3>
                 </div>
             </div>
         </div>`
